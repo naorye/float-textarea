a4f495e71fd9740e7947bce286384a45499d8113
Naor <naor@mediabox.io>
2012-12-29 18:01:05 +0200

    - Adding few solutions in order to find the better one

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 float-textarea
 ==============
 
-jQuery Plugin for draggable and resizable float textarea
+jQuery Plugin for draggable and resizable float textarea.
+This plugin uses jQuery and jQuery-UI Draggable and Resizeable abilities.
--- js/index.js
+++ js/index.js
@@ -25,4 +25,36 @@ $(function() {
                 event.stopPropagation();
             });
     };
+
+    $.fn.textBlock = function() {
+        $tx = this;
+        $dupe = $("<p class='text-block-dupe'></p>").appendTo("body");
+
+        update();
+
+        $tx.on('keydown', function() {
+        setTimeout(update, 0);
+        });
+        $tx.on('propertychange input keyup change', update);
+
+        function update() {
+            var text = $tx.val();
+            $dupe.text(text);
+            $dupe.css("font-size", $tx.css("font-size"));
+
+            var lineHeight = parseFloat($tx.css('line-height'));
+            
+            $tx.css({
+                width: $dupe.width(),
+                height: Math.max($dupe.height(), lineHeight)
+            });
+        }
+
+        // not sure if this is needed, leaving it because
+        // I don't have many browsers to test on
+        $tx.on('scroll', function() {
+            tx.scrollLeft = 0;
+            tx.scrollTop = 0;
+        });
+    };
 });
